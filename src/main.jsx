a548bf6141f27b05a1d89cfa6d3a98acc2b8b4e4
leezii <leezii.atsh@outlook.com>
2018-01-25 23:20:13 +0800

    change import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import Layout from 'src/layout/index'
-import './src/static/less/base.less'
+import Layout from 'layout'
+import './static/less/base.less'
 ReactDom.render(Layout(), document.getElementById('app'))
